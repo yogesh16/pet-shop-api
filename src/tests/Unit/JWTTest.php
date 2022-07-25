e66530081ef4d57932cfdd7c671f520397d005fe
yogesh <yogesh.patel@outlook.in>
2022-07-25 23:55:24 +0530

    ✅ JWT Test created

---
+++ src/tests/Unit/JWTTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use App\Services\JWTService;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class JWTTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_generate_jwt_token()
+    {
+        $user = User::factory()->create();
+        $token = $user->generateToken();
+
+        $this->assertIsString($token);
+    }
+
+    public function test_decode_jwt_token()
+    {
+        $user = User::factory()->create();
+        $token = $user->generateToken();
+
+        $newUser = JWTService::parseToken($token);
+
+        $this->assertEquals($user->id, $newUser->id);
+    }
+}
