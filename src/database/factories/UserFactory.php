cd440297b739ca33479a258ac6157354683b581a
yogesh <yogesh.patel@outlook.in>
2022-07-26 00:15:26 +0530

    ⚡ User migration updated

--- src/database/factories/UserFactory.php
+++ src/database/factories/UserFactory.php
@@ -18,11 +18,15 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => fake()->name(),
+            'uuid' => fake()->uuid(),
+            'first_name' => fake()->firstName(),
+            'last_name' => fake()->lastName(),
             'email' => fake()->safeEmail(),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'address' => fake()->address(),
+            'phone_number' => fake()->phoneNumber()
         ];
     }
 
--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,19 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('uuid', 36);
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->tinyInteger('is_admin')->default(0)->comment('1-Yes, 0-No');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('avatar', 36)->nullable();
+            $table->string('address')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->tinyInteger('is_marketing')->default(0)->comment('1-Yes, 0-No');
+            $table->timestamp('last_login_at')->nullable();
+
             $table->rememberToken();
             $table->timestamps();
         });
