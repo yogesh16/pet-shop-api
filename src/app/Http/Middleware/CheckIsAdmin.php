16bf938acb395af194e44c7a1a1d058b6bd7ca79
yogesh <yogesh.patel@outlook.in>
2022-07-29 15:16:46 +0530

    🔒 Check Admin middleware added

---
+++ src/app/Http/Middleware/CheckIsAdmin.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Traits\ApiResponse;
+use Closure;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class CheckIsAdmin
+{
+    use ApiResponse;
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param Request $request
+     * @param Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return Response|RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if(!empty(trim($request->bearerToken()))){
+            $user = Auth::user();
+            if($user && $user->isAdmin()){
+                return $next($request);
+            }
+        }
+
+        return $this->error('Unauthorized', [], [], 401);
+    }
+}
