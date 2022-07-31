41d61635b91911a8fe61a539e6fd9c35303f2ff9
yogesh <yogesh.patel@outlook.in>
2022-07-31 19:16:16 +0530

    ✅ File upload test created

---
+++ src/tests/Unit/FileTest.php
@@ -0,0 +1,96 @@
+<?php
+
+
+namespace Tests\Unit;
+
+
+use App\Models\Category;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Tests\TestCase;
+
+class FileTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_user_can_upload_file()
+    {
+        Storage::fake('local');
+
+        $file = UploadedFile::fake()->image('pet.png');
+
+        $this->json('POST', '/api/v1/file/upload', ['file' => $file], $this->getHeaders())
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                                      "success",
+                                      "data" => [
+                                          "uuid",
+                                          "name",
+                                          "path",
+                                          "size",
+                                          "type",
+                                          "updated_at",
+                                          "created_at"
+                                      ],
+                                      "error",
+                                      "errors",
+                                      "extra"
+                                  ]);
+
+        $this->assertDatabaseCount('files', 1);
+    }
+
+    public function test_user_can_not_upload_file_without_login()
+    {
+        Storage::fake('local');
+
+        $file = UploadedFile::fake()->image('pet.png');
+
+        $this->json('POST', '/api/v1/file/upload', ['file' => $file], ['Accept' => 'application/json'])
+            ->assertStatus(401)
+            ->assertJsonStructure([
+                                      "success",
+                                      "data",
+                                      "error",
+                                      "errors",
+                                      "trace"
+                                  ]);
+
+        $this->assertDatabaseCount('files', 0);
+    }
+
+
+    //get headers array
+    private function getHeaders($user = null)
+    {
+        if(! isset($user))
+        {
+            $user = $this->getUser();
+        }
+        $token = $user->tokens()->first();
+
+        return [
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer ' . $token->access_token
+        ];
+    }
+
+    //return user
+    private function getUser()
+    {
+        $user = User::factory()->create();
+
+        $userData = [
+            'email' => $user->email,
+            'password' => 'password'
+        ];
+
+        $this->json('POST', '/api/v1/user/login', $userData, ['Accept' => 'application/json'])
+            ->assertStatus(200);
+
+        return $user;
+    }
+}
