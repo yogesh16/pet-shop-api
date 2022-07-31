fb94ddb99c0dbeb9c2d0a96fa86f13fac537e58b
yogesh <yogesh.patel@outlook.in>
2022-07-31 23:05:40 +0530

    ✅ refactored code

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,6 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return \App\Http\Resources\ProductResource::collection(\App\Models\Product::paginate(5));
-    //return view('welcome');
+    return view('welcome');
 });
